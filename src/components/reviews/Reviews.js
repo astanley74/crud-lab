0c87dedf93b054bd70743d401c2e11becb0b1e23
astanley74 <stanley.377@buckeyemail.osu.edu>
2021-04-14 16:02:33 -0400

    Done.

--- src/components/reviews/Reviews.js
+++ src/components/reviews/Reviews.js
@@ -3,9 +3,14 @@ import Review from './Review';
 
 class Reviews extends Component {
   render() {
+    const restReviews = this.props.reviews.filter(review => review.restaurantId === this.props.restaurantId)
+    const listOfReviews = restReviews.map((review, index) => {
+      return <Review key={index} review={review} deleteReview={this.props.deleteReview}/>
+    })
+
     return (
       <ul>
-        Reviews
+        {listOfReviews}
       </ul>
     );
   }
